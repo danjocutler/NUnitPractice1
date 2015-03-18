8ac89b0ea93a78c2ae5b5ad0eb919406a7a1b8e4
danjocutler <danjocutler@gmail.com>
2015-03-18 14:50:04 +0000

    passed my first NUnit test

---
+++ NUnitPractice1/NUnitPractice1Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Bank
+{
+    /// <summary>
+    /// A test class for ...
+    /// </summary>
+    [TestFixture]
+    public class AccountTest
+    {
+        #region Setup and Tear down
+        /// <summary>
+        /// This runs only once at the beginning of all tests and is used for all tests in the 
+        /// class.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void InitialSetup()
+        {
+
+        }
+
+        /// <summary>
+        /// This runs only once at the end of all tests and is used for all tests in the class.
+        /// </summary>
+        [TestFixtureTearDown]
+        public void FinalTearDown()
+        {
+
+        }
+
+        /// <summary>
+        /// This setup funcitons runs before each test method
+        /// </summary>
+        [SetUp]
+        public void SetupForEachTest()
+        {
+        }
+
+        /// <summary>
+        /// This setup funcitons runs after each test method
+        /// </summary>
+        [TearDown]
+        public void TearDownForEachTest()
+        {
+        }
+        #endregion
+
+        [Test]
+        public void TransferFunds()
+        {
+            // Step 1 - Arrange
+            Account source = new Account();
+            source.Deposit(200m);
+
+            // Step 2 - Act
+            Account destination = new Account();
+            destination.Deposit(150m);
+
+            source.TransferFunds(destination, 100m);
+
+            // Step 3 - Assert  
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+    }
+}
---
+++ NUnitPractice1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Bank
+{
+    public class Account
+    {
+        [STAThread]
+        static void Main()
+        { 
+        }
+
+        private decimal balance;
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance -= amount;
+        }
+
+        public void TransferFunds(Account destination, decimal amount)
+        {
+            destination.Deposit(amount);
+            Withdraw(amount);
+        }
+
+        public decimal Balance
+        {
+            get { return balance;  }
+        }
+    }
+}
